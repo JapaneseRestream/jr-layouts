955dcdc19d621de0fd75b5b9e502688ae527c511
hoishin <hoishinxii@gmail.com>
2018-05-17 12:28:17 -0600

    add translation for game titles

--- extension/schedule.js
+++ extension/schedule.js
@@ -4,7 +4,7 @@ const FETCH_SCHEDULE_INTERVAL = 60 * 1000;
 
 module.exports = nodecg => {
 	const scheduleRep = nodecg.Replicant('schedule');
-	const { trackerUrl } = nodecg.bundleConfig;
+	const { trackerUrl, translationUrl } = nodecg.bundleConfig;
 
 	if (!trackerUrl) {
 		nodecg.log.info("Tracker URL is not provided. Schedule won't be fetched");
@@ -15,27 +15,41 @@ module.exports = nodecg => {
 	setInterval(fetchHoraroSchedule, FETCH_SCHEDULE_INTERVAL);
 
 	function fetchHoraroSchedule() {
-		const url = trackerUrl;
-
-		request.get(url).end((err, { body }) => {
+		request.get(translationUrl).end((err, { text }) => {
 			if (err) {
-				nodecg.log.error("Couldn't update Horaro schedule.");
-				return;
+				nodecg.log.error("Couldn't fetch translation info.");
 			}
 
-			scheduleRep.value = body.map((run, index) => {
-				const { fields, pk } = run;
-				return {
-					index,
-					pk,
-					scheduled: fields.starttime,
-					game: fields.name || '',
-					category: fields.category || 'Any%',
-					console: fields.console || '',
-					runners: fields.deprecated_runners || '',
-					english: fields.name || '',
-					commentator: ''
-				};
+			let translation;
+			try {
+				translation = JSON.parse(text);
+			} catch (err) {
+				translation = [];
+			}
+
+			request.get(trackerUrl).end((err, { body }) => {
+				if (err) {
+					nodecg.log.error("Couldn't update Horaro schedule.");
+					return;
+				}
+
+				scheduleRep.value = body.map((run, index) => {
+					const { fields, pk } = run;
+					const gameTranslation = translation.find(
+						item => item.en.toLowerCase() === fields.name.toLowerCase()
+					);
+					return {
+						index,
+						pk,
+						scheduled: fields.starttime,
+						game: gameTranslation ? gameTranslation.jp || '' : '',
+						category: fields.category || 'Any%',
+						console: fields.console || '',
+						runners: fields.deprecated_runners || '',
+						english: fields.name || '',
+						commentator: ''
+					};
+				});
 			});
 		});
 	}
