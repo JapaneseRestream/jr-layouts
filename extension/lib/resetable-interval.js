428e8c3f4e82bb3e2658b03111f62045d50b641b
hoishin <hoishinxii@gmail.com>
2018-05-20 21:30:41 -0700

    refactor resetable interval

--- extension/lib/resetable-interval.js
+++ extension/lib/resetable-interval.js
@@ -1,48 +1,41 @@
-const is = require('@sindresorhus/is')
+const is = require('@sindresorhus/is');
 
 class ResetableInterval {
 	constructor(fn, delay) {
-		this.fn = this.validFn(fn)
-		this.delay = this.validDelay(delay)
+		this.fn = this._validFn(fn);
+		this.delay = this._validDelay(delay);
 	}
 
 	start(...args) {
-		this.interval = setInterval(this.fn, this.delay, ...args)
+		this.interval = setInterval(this.fn, this.delay, ...args);
 	}
 
 	call(...args) {
-		return this.fn(...args)
+		return this.fn(...args);
 	}
 
-	reset(fn, delay, ...args) {
-		if (!is.nullOrUndefined(fn)) {
-			this.fn = this.validFn(fn)
-		}
-		if (!is.nullOrUndefined(delay)) {
-			this.delay = this.validDelay(delay)
-		}
-		this.stop()
-		this.interval = setInterval(this.fn, this.delay, ...args)
+	reset(...args) {
+		this.stop();
+		this.start(...args);
 	}
 
 	stop() {
-		clearInterval(this.interval)
-		delete this.interval
+		clearInterval(this.interval);
 	}
 
-	validFn(fn) {
+	_validFn(fn) {
 		if (is.function(fn)) {
-			return fn
+			return fn;
 		}
-		throw new TypeError('Interval callback should be function')
+		throw new TypeError('Interval callback should be function');
 	}
 
-	validDelay(delay) {
+	_validDelay(delay) {
 		if (is.number(delay)) {
-			return delay
+			return delay;
 		}
-		throw new TypeError('Interval delay should be number')
+		throw new TypeError('Interval delay should be number');
 	}
 }
 
-module.exports = ResetableInterval
+module.exports = ResetableInterval;
