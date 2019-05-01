353ce4ab4ee1fc392fdb8ef6d104bab4b814af2f
Hoishin <hoishinxii@gmail.com>
2019-05-01 12:02:01 +0900

    Use inline base64 font from CSS

--- jr-layouts/src/graphics/main.tsx
+++ jr-layouts/src/graphics/main.tsx
@@ -1,3 +1,4 @@
+import './fonts.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Main} from './components/main';
--- jr-layouts/webpack.config.ts
+++ jr-layouts/webpack.config.ts
@@ -1,9 +1,9 @@
 import path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import merge from 'webpack-merge';
 import globby from 'globby';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
-import CleanWebpackPlugin from 'clean-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -13,7 +13,6 @@ const base: webpack.Configuration = {
 	resolve: {
 		extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
 	},
-	plugins: [new CleanWebpackPlugin()],
 	devtool: 'cheap-source-map',
 };
 
@@ -49,7 +48,7 @@ const generateBrowserConfig = (
 					],
 				},
 				{
-					test: /\.(png)$/u,
+					test: /\.png$/u,
 					loaders: [
 						{
 							loader: 'file-loader',
@@ -59,9 +58,20 @@ const generateBrowserConfig = (
 						},
 					],
 				},
+				{
+					test: /\.css$/u,
+					loaders: [MiniCssExtractPlugin.loader, 'css-loader'],
+				},
+				{
+					test: /\.woff2$/u,
+					loaders: ['url-loader'],
+				},
 			],
 		},
 		plugins: [
+			new MiniCssExtractPlugin({
+				filename: '[name].[contenthash].css',
+			}),
 			...Object.keys(entryObject).map(
 				(entryName) =>
 					new HtmlWebpackPlugin({
