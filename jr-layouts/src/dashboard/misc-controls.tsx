4995012b5e8d0592d33f6e8fb354892874cdafbc
Keiichiro Amemiya <hoishinxii@gmail.com>
2019-07-05 05:54:15 +0900

    reflect current value in channel name form

--- jr-layouts/src/dashboard/misc-controls.tsx
+++ jr-layouts/src/dashboard/misc-controls.tsx
@@ -2,11 +2,18 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import {Button, TextField} from '@material-ui/core';
 import {Message} from '../constants';
+import {useReplicant} from '../use-nodecg/use-replicant';
 
 const targetChannelRep = nodecg.Replicant<string>('target-channel');
 
 const App: React.FunctionComponent = () => {
-	const [targetChannel, setTargetChannel] = useState('');
+	const [targetChannel, setTargetChannel] = useState<string | null>(null);
+	const [targetChannelRepValue, setTargetChannelRep] = useReplicant(
+		targetChannelRep,
+		null,
+	);
+	const latestChannel = targetChannel || targetChannelRepValue || '';
+
 	return (
 		<>
 			<Button
@@ -18,13 +25,14 @@ const App: React.FunctionComponent = () => {
 				レイアウトのプレイヤーを再読込する
 			</Button>
 			<TextField
+				value={latestChannel}
 				onChange={(e) => setTargetChannel(e.target.value)}
 				placeholder='うつすTwitchチャンネル'
 			/>
 			<Button
 				variant='contained'
 				onClick={() => {
-					targetChannelRep.value = targetChannel;
+					setTargetChannelRep(latestChannel);
 				}}
 			>
 				変える
