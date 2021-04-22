8a41f3d5e0360ea4340f1bd4ec00f3c31a8fa266
Keiichiro Amemiya <hoishinxii@gmail.com>
2021-04-22 11:56:31 +0900

    fix: layout adjustment

--- src/browser/dashboard/admin.tsx
+++ src/browser/dashboard/admin.tsx
@@ -1,9 +1,18 @@
 import "modern-normalize";
 
+import styled from "styled-components";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import {useReplicant} from "../shared/use-nodecg/use-replicant";
+
+const Container = styled.div`
+	margin: 8px;
+
+	display: grid;
+	grid-gap: 8px;
+	grid-auto-flow: row;
+`;
 
 const App: React.FunctionComponent = () => {
 	const [obsAutoRecording, setObsAutoRecording] = useReplicant(
@@ -13,16 +22,18 @@ const App: React.FunctionComponent = () => {
 		return null;
 	}
 	return (
-		<div>
-			<label>OBS自動録画オン</label>
-			<input
-				type='checkbox'
-				checked={obsAutoRecording}
-				onChange={(e) => {
-					setObsAutoRecording(e.target.checked);
-				}}
-			></input>
-		</div>
+		<Container>
+			<div>
+				<label>OBS自動録画オン</label>
+				<input
+					type='checkbox'
+					checked={obsAutoRecording}
+					onChange={(e) => {
+						setObsAutoRecording(e.target.checked);
+					}}
+				></input>
+			</div>
+		</Container>
 	);
 };
 
